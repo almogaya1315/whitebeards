a893fd96318df0cf828a7306d53f4b136bf99f6c
Lior Matsliah <liormatsliah1985@gmail.com>
2025-03-23 10:49:40 +0200

    init

---
+++ src/app/catalog/catalog.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+
+import { UserRepositoryService } from "../services/user-repository.service"
+import { CatalogRepositoryService } from './catalog-repository.service';
+import { IClass } from './class.model';
+
+@Component({
+  styleUrls: ['./catalog.component.css'],
+  templateUrl: './catalog.component.html'
+})
+export class CatalogComponent {
+  classes: IClass[] = [];
+  visibleClasses: IClass[] = [];
+
+  constructor(public userRepo: UserRepositoryService, private catalogRepo: CatalogRepositoryService) { }
+
+  ngOnInit() {
+    this.catalogRepo.getCatalog()
+      .subscribe((classes: IClass[]) => { this.classes = classes; this.applyFilter('') });
+  }
+
+  enroll(classToEnroll: IClass) {
+    classToEnroll.processing = true;
+    this.userRepo.enroll(classToEnroll.classId)
+      .subscribe({
+        error: (err) => { console.error(err); classToEnroll.processing = false },
+        complete: () => { classToEnroll.processing = false; classToEnroll.enrolled = true; },
+      });
+  }
+
+  drop(classToDrop: IClass) {
+    classToDrop.processing = true;
+    this.userRepo.drop(classToDrop.classId)
+      .subscribe({
+        error: (err) => { console.error(err); classToDrop.processing = false },
+        complete: () => { classToDrop.processing = false; classToDrop.enrolled = false; }
+      });
+  }
+
+  applyFilter(filter: string) {
+    if (!filter) {
+      this.visibleClasses = this.classes;
+      return;
+    }
+
+    if (filter === 'GEN') {
+      this.showOnlyGeneralCourses();
+    } else {
+      this.visibleClasses = this.classes.filter(c => c.course.courseNumber.startsWith(filter));
+    }
+  }
+
+  private showOnlyGeneralCourses(){
+    this.visibleClasses = this.classes.filter(c =>
+      !c.course.courseNumber.startsWith('CH') &&
+      !c.course.courseNumber.startsWith('PO') &&
+      !c.course.courseNumber.startsWith('SP'));
+  }
+}
